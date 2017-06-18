959a9d6f888554e752ed32fb4e3765bd22aa0ee8
Renato Stauffer <renatostauffer@renatos-macbook-pro.local>
2017-06-18 17:34:54 +0200

    Initial Commit

---
+++ RSUtils/RSUtils.h
@@ -0,0 +1,19 @@
+//
+//  RSUtils.h
+//  RSUtils
+//
+//  Created by Renato Stauffer on 18.06.17.
+//  Copyright © 2017 Renato Stauffer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RSUtils.
+FOUNDATION_EXPORT double RSUtilsVersionNumber;
+
+//! Project version string for RSUtils.
+FOUNDATION_EXPORT const unsigned char RSUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RSUtils/PublicHeader.h>
+
+
